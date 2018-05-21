883d889519f48ac5ee7d28a274d96605e0efabf8
Immiora <immiora@gmail.com>
2018-05-21 09:46:12 +0200

    upload_get_freq_data

--- python/get_freq_int.py
+++ python/get_freq_int.py
@@ -1,10 +1,11 @@
 import numpy as np
+import pandas as pd
 
 def apply_hanning(x, M):
     return x * np.atleast_2d(np.hanning(M)).T
 
 def trim_fft(x):
-    return x[:len(x) / 2] * 2
+    return x[:int(len(x) / 2)] * 2
 
 def compute_fft(x, sr, axis=0, use_window=False):
     l = x.shape[axis]
@@ -31,14 +32,39 @@ def get_max_per_band(s, f):
     return out
 
 def process_data(signal, sr=250):
-    '''
-    Get max fft per band
-    :param signal: matrix win x channels; sr: sampling rate
-    :return: dictionary: bands -> electrodes -> freq, int
-    '''
     x, f = compute_fft(signal, sr)
     return get_max_per_band(x, f)
 
-# signal = np.loadtxt('./data/sample.csv', delimiter=',')
+# signal = np.loadtxt('./data/testfile_4.csv', delimiter=',')
 # output = process_data(signal)
+
 #
+def get_preprocessed_data(filename='./data/testfile_4.csv', wlen=128, overlap=64):
+    '''
+    Get a list of dictionaries, each holds freq and int per channel and freq band
+    Inside get fft per window (wlen) with overlap (overlap)
+    :param filename: data filename, assumes space as delimiter
+    :return: list of dicts
+    '''
+
+    with open(filename) as f:
+        data = f.readlines()
+
+    data = [i.split('\t') for i in data]
+
+    for id, d in enumerate(data):
+        if len(d) > 1:
+            data = data[id:]
+            break
+
+    D = pd.DataFrame(data)
+
+    data = np.abs(np.array(D[np.arange(1,9)]).astype(np.float))
+    n = data.shape[0]
+
+    dicts = []
+
+    for w in np.arange(0, n, overlap):
+        dicts.append(process_data(data[w:w+wlen,:]))
+
+    return dicts[:-3]
